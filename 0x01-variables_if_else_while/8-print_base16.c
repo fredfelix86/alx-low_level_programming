256908b51960622b4f8a18ad9952b2a8ed0991a5
fredfelix86 <fredfeliclear>
2022-10-17 07:52:56 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char c, n;
+	char c;
 
-	for (c = '0'; n <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
 
-	for (c = 'a'; n <= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
