ce81fa3c31835e65fd4860bbd8f4750751cc97bd
fredfelix86 <fredfeliclear>
2022-10-17 07:41:33 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a hexadecimal string
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; n <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; n <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
