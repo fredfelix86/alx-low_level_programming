73788f48ef60c63c716361cf38087c944fd9fac8
fredfelix86 <fredfeliclear>
2022-10-18 20:03:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main,h"
+
+/**
+ * print-alphabet - print all alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	putchar('\n');
+}
