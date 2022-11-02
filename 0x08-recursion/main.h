1cd2cc0d6286c68c2d8142e8180be5f3dcf178a4
fredfelix86 <fredfeliclear>
2022-11-02 08:40:49 -0700

    Task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+/**
+ * File: main.h
+ * Author: Fred
+ * Description: Header file containing prototypes for all functions used
+ * in the 0x08-recursion directory.
+ */
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,5 +17,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int factorial(int n);
+int is_palindrome(char *s);
 
-#endif /*Fredfelix*/
+#endif
