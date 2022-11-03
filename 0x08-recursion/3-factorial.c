646198e0d6a23f8e594e34bb80289a361b1918c5
fredfelix86 <fredfeliclear>
2022-11-03 00:51:03 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - Calculate the factorial of a new number
+ * @n: The number to calculate the factorial
+ *
+ * Return: Integer value
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n <= 1)
+		retuen(1);
+
+	return (n * factorial(n - 1));
+}
